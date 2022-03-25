f390bcd20b194be69ce40aebd2b6893146489aab
Rengasther-BrN <brunoaffonsomarques@gmail.com>
2022-03-25 15:57:36 -0300

    Enviando Header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 
@@ -15,7 +18,10 @@ import { MatIconModule } from '@angular/material/icon';
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
