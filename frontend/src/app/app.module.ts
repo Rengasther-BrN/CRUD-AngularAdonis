c4f488b9bfacb7da8a0b55c186859af489a8639b
Bruno Marques <brunoaffonsomarques@gmail.com>
2022-04-30 10:30:52 -0300

    Version 2.1
    frontend inclusion

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { AboutComponent } from './componets/pages/about/about.component';
+import { HomeComponent } from './componets/pages/home/home.component';
+import { NewMomentsComponent } from './componets/pages/new-moments/new-moments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    NewMomentsComponent
   ],
   imports: [
     BrowserModule,
