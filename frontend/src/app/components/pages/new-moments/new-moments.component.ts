3abc5ecfbc622bdc374481b92cca0a66b0c5e21a
Bruno Marques <brunoaffonsomarques@gmail.com>
2022-06-02 19:11:21 -0300

    2.4

--- frontend/src/app/components/pages/new-moments/new-moments.component.ts
+++ frontend/src/app/components/pages/new-moments/new-moments.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Moment } from 'src/app/moment'
+
+import { MomentService } from 'src/app/services/moment.service';
+
 @Component({
   selector: 'app-new-moments',
   templateUrl: './new-moments.component.html',
@@ -8,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentsComponent implements OnInit {
   btnText = 'Compartilhar!';
 
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  async createHandler(moment : Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title)
+    formData.append('description', moment.description)
+
+    if (moment.image) {
+      formData.append('image',moment.image)
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
   }
+
+ 
 
 }
